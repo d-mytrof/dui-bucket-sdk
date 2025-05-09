da1aa2f67922f936ceed5bc5dd24c0f248a37763
Dmytro Mytrofanov <dmitrof.development@gmail.com>
2025-05-10 00:16:49 +0300

    initial commit

--- src/Laravel/DuiBucketServiceProvider.php
+++ src/Laravel/DuiBucketServiceProvider.php
@@ -1,9 +1,11 @@
 <?php
+
 namespace dmytrof\DuiBucketSDK\Laravel;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Log;
 use Psr\Log\LoggerInterface;
+use dmytrof\DuiBucketSDK\Config\Config as DuiConfig;
 use dmytrof\DuiBucketSDK\DuiBucketSDK;
 use dmytrof\DuiBucketSDK\Http\BucketClient;
 use dmytrof\DuiBucketSDK\Error\ErrorHandler;
@@ -12,37 +14,35 @@ use dmytrof\DuiBucketSDK\Logging\FileLogger;
 class DuiBucketServiceProvider extends ServiceProvider
 {
     /**
-     * Register the SDK instance, HTTP client, and logging channel.
+     * Register Config, SDK, HTTP client, and custom log channel.
      */
     public function register()
     {
         // Merge the package configuration file
-        $this->mergeConfigFrom(__DIR__.'/config/dui-bucket.php', 'dui-bucket');
+        $this->mergeConfigFrom(__DIR__ . '/config/dui-bucket.php', 'dui-bucket');
 
-        // Register the core SDK singleton
+        // Bind the Config instance (array → Config object)
+        $this->app->singleton(DuiConfig::class, function ($app) {
+            return new DuiConfig($app['config']->get('dui-bucket'));
+        });
+
+        // Bind the core SDK singleton (accepts array internally)
         $this->app->singleton('dui-bucket-sdk', function ($app) {
-            $config = $app['config']->get('dui-bucket');
-            return new DuiBucketSDK([
-                'api_url'        => $config['api_url'],
-                'api_key'        => $config['api_key'],
-                'default_bucket' => $config['default_bucket'],
-                'encryption'     => $config['encryption'],
-            ]);
+            return new DuiBucketSDK($app['config']->get('dui-bucket'));
         });
 
-        // Register the HTTP client used by the SDK and error handler
+        // Bind the HTTP client for SDK and error reporting
         $this->app->singleton(BucketClient::class, function ($app) {
-            $config = $app['config']->get('dui-bucket');
-            return new BucketClient($config['api_url'], $config['api_key']);
+            $config = $app->make(DuiConfig::class);
+            $logger = $app->make(LoggerInterface::class);
+            return new BucketClient($config, $logger);
         });
 
-        // Extend Laravel logging with the custom FileLogger channel
+        // Extend Laravel logging with a custom FileLogger channel
         Log::extend('dui_bucket', function ($app, $config) {
-            // Allow overriding via the logging channel config
             $apiUrl = $config['api_url'] ?? $app['config']->get('dui-bucket.api_url');
             $apiKey = $config['api_key'] ?? $app['config']->get('dui-bucket.api_key');
             $level  = $config['level']   ?? 'error';
-
             return new FileLogger($apiUrl, $apiKey, $level);
         });
     }
@@ -52,27 +52,27 @@ class DuiBucketServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Publish the config file when running in the console
+        // Publish the config file when running in console
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/config/dui-bucket.php' => config_path('dui-bucket.php'),
+                __DIR__ . '/config/dui-bucket.php' => config_path('dui-bucket.php'),
             ], 'dui-bucket-config');
         }
 
         // Load package config
-        $config = $this->app['config']->get('dui-bucket');
+        $cfg = $this->app['config']->get('dui-bucket');
 
-        // Choose the appropriate logger: custom channel or default PSR-3 logger
-        if (!empty($config['log_enabled'])) {
-            $logger = Log::channel($config['log_channel']);
+        // Choose logger: custom channel or default PSR-3 logger
+        if (!empty($cfg['log_enabled'])) {
+            $logger = Log::channel($cfg['log_channel']);
         } else {
             $logger = $this->app->make(LoggerInterface::class);
         }
 
-        // Resolve the HTTP client for error reporting
+        // Resolve the HTTP client
         $client = $this->app->make(BucketClient::class);
 
-        // Register the global exception handler with the logger and HTTP client
+        // Register the global exception handler
         ErrorHandler::register($logger, $client);
     }
 }
