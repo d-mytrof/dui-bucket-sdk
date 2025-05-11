e1b4be40be55db3c8dce67e6c1b4eb63d9c2077c
Dmytro Mytrofanov <dmitrof.development@gmail.com>
2025-05-11 22:32:44 +0300

    initial commit

---
+++ src/Laravel/DuiExceptionHandler.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright Copyright © 2025 Dmytro Mytrofanov
+ * @package dui-bucket-sdk
+ * @version 1.0.0
+ */
+
+namespace dmytrof\DuiBucketSDK\Laravel;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class DuiExceptionHandler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<Throwable>>
+     */
+    protected $dontReport = [
+        // Add exception types here if you don't want them reported
+    ];
+
+    /**
+     * Report or log an exception.
+     */
+    public function report(Throwable $e): void
+    {
+        parent::report($e);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $e)
+    {
+        return parent::render($request, $e);
+    }
+}
